090a69faca52ef1039deac868d1d8182515ed34c
Sho3leh <abusholeh13@gmail.com>
2024-11-02 19:08:24 +0300

    Course.cs

--- StudentCourseProject/Course.cs
+++ StudentCourseProject/Course.cs
@@ -6,11 +6,11 @@
         public string CourseName { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public int CategoryId { get; set; } // البرمجة أو غيرها
+        public int CategoryId { get; set; } 
         public decimal Price { get; set; }
-        public bool IsDeleted { get; set; } = false; // للحذف اللوجيكي
+        public bool IsDeleted { get; set; } = false; 
 
-        // علاقة Many-to-Many مع Student
+        
         public ICollection<Student> Students { get; set; }
     }
 
